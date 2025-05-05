df1951908858d9aa19be964ec81d0b0324917676
imakabby <imaalcatrax@gmail.com>
2025-05-05 22:12:38 +0800

    fdfd

--- assets/js/script.js
+++ assets/js/script.js
@@ -116,37 +116,6 @@ document.addEventListener('DOMContentLoaded', () => {
     updateButtons();
 });
 
-// * MEMBUAT TOMBOL NAVIGASI ----
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    
-    // Animasi untuk hamburger menu
-    const spans = menuToggle.querySelectorAll('span');
-    spans[0].classList.toggle('rotate-45');
-    spans[1].classList.toggle('opacity-0');
-    spans[2].classList.toggle('rotate-negative-45');
-});
-
-// ? Tambahkan CSS untuk animasi hamburger
-const style = document.createElement('style');
-style.textContent = `
-    .rotate-45 {
-        transform: rotate(45deg) translate(6px, 6px);
-    }
-    
-    .opacity-0 {
-        opacity: 0;
-    }
-    
-    .rotate-negative-45 {
-        transform: rotate(-45deg) translate(6px, -6px);
-    }
-`;
-document.head.appendChild(style); 
-
 // Tambahkan listener untuk dark mode toggle
 document.addEventListener('DOMContentLoaded', function() {
     const darkModeToggles = document.querySelectorAll('.theme-switch input[type="checkbox"]');
